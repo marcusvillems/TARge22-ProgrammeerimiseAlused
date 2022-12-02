1d4e9441281b03aaf03f02ccb2ecf752d4d329f7
Opilane TTHK <opilane@tthk.ee>
2022-12-02 17:07:16 +0200

    Add Python files here

---
+++ Greeting.py
@@ -0,0 +1,7 @@
+def greeting():
+    name = input("What is your name?")
+    print("Hello, " + name + "!")
+    
+    
+if __name__ == "__main__":
+        greeting()
---
+++ Sum.py
@@ -0,0 +1,8 @@
+def ask_and_sum_two_numbers():
+    number1 = float(input("Enter first number: "))
+    number2 = float(input("Enter second number: "))
+    two_number_sum = number1 + number2
+    print("number1" + " + " + "number2 = " + str(two_number_sum))
+    
+if __name__ == "__main__":
+    ask_and_sum_two_numbers()
